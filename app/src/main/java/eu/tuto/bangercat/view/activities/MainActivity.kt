0ffc694c371c26c1a4ae0b291da8e1a3001ce9c7
Domson12 <dominikmz12@op.pl>
2022-05-14 11:41:47 +0200

    First commit, basic app layout

---
+++ app/src/main/java/eu/tuto/bangercat/view/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package eu.tuto.bangercat.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.tuto.bangercat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
