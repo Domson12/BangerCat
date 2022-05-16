a454b62760f2720d55de88381c68dc3ec83ebae3
Domson12 <dominikmz12@op.pl>
2022-05-16 18:08:51 +0200

    add button functionality, activity_add_music opens, no database actions yet included

--- app/src/main/java/eu/tuto/bangercat/view/activities/AddMusicActivity.kt
+++ app/src/main/java/eu/tuto/bangercat/view/activities/AddMusicActivity.kt
@@ -3,10 +3,13 @@ package eu.tuto.bangercat.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import eu.tuto.bangercat.R
+import eu.tuto.bangercat.databinding.ActivityAddMusicBinding
+
+
 
 class AddMusicActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_musis)
+        setContentView(R.layout.activity_add_music)
     }
 }
--- app/src/main/java/eu/tuto/bangercat/view/activities/MainActivity.kt
+++ app/src/main/java/eu/tuto/bangercat/view/activities/MainActivity.kt
@@ -1,12 +1,36 @@
 package eu.tuto.bangercat.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import eu.tuto.bangercat.R
+import eu.tuto.bangercat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.addButton.setOnClickListener {
+            addButtonActivity()
+            Log.e("works", "da clicked")
+        }
+
     }
+
+    private fun addButtonActivity() {
+        val intent = Intent(this, AddMusicActivity::class.java)
+        startActivity(intent)
+        Log.e("works", "da clicked")
+    }
+
 }
